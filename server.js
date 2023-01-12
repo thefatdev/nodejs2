3266cea38458d2a49d67d76263fd03a637e1bf11
webgiaretoanquoc <webgiaretoanquoc.com@gmail.com>
2023-01-12 17:14:25 +0700

    add new code first

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.use('/public', express.static(path.join(__dirname, '/public')))
+
+app.get('/', (req, res) => {
+    var duongDanFile = path.join(__dirname, '/public/home.html')
+  res.sendFile(duongDanFile)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
