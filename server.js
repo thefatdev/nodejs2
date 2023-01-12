880c87bcf9aae85fdfb40f40aae5e7c63b3eb03b
webgiaretoanquoc <webgiaretoanquoc.com@gmail.com>
2023-01-12 17:57:38 +0700

    no

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const path = require('path')
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
-    var duongDanFile = path.join(__dirname, 'home.html')
+    var duongDanFile = path.join(__dirname, '/public/home.html')
   res.sendFile(duongDanFile)
 })
 
-app.listen(process.env.PORT, function(){
+app.listen(3000, function(){
 
 })
